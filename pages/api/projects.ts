1c232a9ac40023bab20da0197898539162cacf0a
folorunsho-tech <folorunshoibr.com>
2023-02-28 17:34:26 +0100

    Updated project api

--- pages/api/projects.ts
+++ pages/api/projects.ts
@@ -1,22 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import projects from "../../projects.json";
-type Data = {
-  category: string;
-  projects: {
-    name: string;
-    img: {
-      main: string;
-    };
-    link: string;
-    code_link: string;
-    tags: string[];
-  }[];
-}[];
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
   res.status(200).json(projects);
 }
