47c26c7cf5129ed0e993e5ac243edeaea0896643
folorunsho-tech <folorunshoibr.com>
2023-04-09 10:30:56 +0100

    Updated project info

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -35,7 +35,7 @@ const ProjectCard = ({ img, name, tags, link, category }: ProjectCardProps) => {
         width={400}
         height={100}
       />
-      <h2 className={`${oswald.className} text-3xl`}>{name}</h2>
+      <h2 className={`${oswald.className} text-3xl capitalize`}>{name}</h2>
       <p
         className={`${poppins.className} flex gap-4 items-center text-lg text-gray-400 pb-4`}
       >
